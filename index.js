4862b82d360ec31bd31749370f7ac410269f8d52
MuhammadZeeshan03 <muhamadzeeshan03@gmail.com>
2023-08-08 17:02:40 +0500

    database connection inprogress

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const sequelize = require("./db/database");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.json());
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hi, this is the root route');
+app.get("/", (req, res) => {
+  res.send("Hi, this is the root route");
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+sequelize
+  .sync()
+  .then((result) => {
+    console.log(result);
+    
+    app.listen(3000)})  
+  .catch((err) => {
+    console.log(err);
+  });
