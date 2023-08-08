ad52771c9dbb31f9490b826f5256abb66e308f0f
MuhammadZeeshan03 <muhamadzeeshan03@gmail.com>
2023-08-08 20:39:45 +0500

    Delete Product Functionality added

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -1,59 +1,71 @@
 const product = require("../models/productModel");
 
 exports.getProducts = (req, res) => {
-    product
-        .findAll()
-        .then((products) => {
-        res.json(products);
-        })
-        .catch((err) => console.log(err));
-    }
-    
-    exports.postProduct = (req, res) => {
-        const name = req.body.name;
-        const description = req.body.description;
-        const price = req.body.price;
-        const category = req.body.category;
-        const tags = req.body.tags;
-        console.log(name + description + price + category + tags);
-        product
-          .create({
-            name: name,
-            description: description,
-            price: price,
-            category: category,
-            tags: tags,
-          })
-          .then((result) => {
-            console.log(result);
-            res.json(result);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
+  product
+    .findAll()
+    .then((products) => {
+      res.json(products);
+    })
+    .catch((err) => console.log(err));
+};
 
+exports.postProduct = (req, res) => {
+  const name = req.body.name;
+  const description = req.body.description;
+  const price = req.body.price;
+  const category = req.body.category;
+  const tags = req.body.tags;
+  console.log(name + description + price + category + tags);
+  product
+    .create({
+      name: name,
+      description: description,
+      price: price,
+      category: category,
+      tags: tags,
+    })
+    .then((result) => {
+      console.log(result);
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-      exports.updateProduct = (req, res) => {  // update product it requires id and all the fields 
-        const id = req.params.id;
-        const name = req.body.name;
-        const description = req.body.description;
-        const price = req.body.price;
-        const category = req.body.category;
-        const tags = req.body.tags;
-        product
-          .findByPk(id)
-          .then((product) => {
-            product.name = name;
-            product.description = description;
-            product.price = price;
-            product.category = category;
-            product.tags = tags;
-            return product.save();
-          })
-          .then((result) => {
-            res.json(result);
-          })
-          .catch((err) => console.log(err));
-      }
-      
+exports.updateProduct = (req, res) => {
+  // update product it requires id and all the fields
+  const id = req.params.id;
+  const name = req.body.name;
+  const description = req.body.description;
+  const price = req.body.price;
+  const category = req.body.category;
+  const tags = req.body.tags;
+  product
+    .findByPk(id)
+    .then((product) => {
+      product.name = name;
+      product.description = description;
+      product.price = price;
+      product.category = category;
+      product.tags = tags;
+      return product.save();
+    })
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.deleteProduct = (req, res) => {
+  const id = req.params.id;
+  product
+    .findByPk(id)
+    .then((product) => {
+      return product.destroy();
+    })
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => console.log(err));
+};
--- routes/product.js
+++ routes/product.js
@@ -7,6 +7,7 @@ const productController = require('../Controllers/productController');
 router.get('/getProducts',productController.getProducts);
 router.post('/postProduct',productController.postProduct);
 router.put('/updateProduct/:id',productController.updateProduct);
+router.delete('/deleteProduct/:id',productController.deleteProduct);
 
 
 module.exports = router;
